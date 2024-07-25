3a399bf5174913b1a9ca0aa5bb927c485eaa6352
edineusa <edineusa08@gmail.com>
2024-07-25 12:40:31 -0300

    first commit

---
+++ src/main/java/desafioApiRest/Application.java
@@ -0,0 +1,13 @@
+package desafioApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDeProjetoDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDeProjetoDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/desafioApiRest/DesafioDeProjetoDioApplicationTests.java
@@ -0,0 +1,13 @@
+package desafioApiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDeProjetoDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
